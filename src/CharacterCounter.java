6279fbbc001aa6d304f76fb28139acaeb6f3d5c9
darango-o <d.arango@globant.com>
2023-08-31 21:21:28 -0500

    Some tweaks

--- src/CharacterCounter.java
+++
@@ -1,5 +0,0 @@
-public class CharacterCounter {
-    public int charCount(String word){
-        return word.length();
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
         String env = System.getenv("ENV");
         System.out.println("Env: "+env);
-        CharacterCounter cc = new CharacterCounter();
-        System.out.println("The length of the environment word is: " + cc.charCount(env));
+        System.out.println("The length of the environment word is: " + charCount(env));
+    }
+
+    private static int charCount(String word){
+        return word.length();
     }
 }
